11a713f1e0632efa11c080ff85264c9873acaefc
Darrell-Conklin <dmconklin84@gmail.com>
2020-05-10 17:08:03 -0500

    added react-redux, redux, redux-logger as dependencies, set up redux store, reducer and set up logger

---
+++ app/src/reducer/reducer.js
@@ -0,0 +1,12 @@
+const initialState = [
+    {
+        name: 'Papa Smurf'
+    }
+]
+
+export function reducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
